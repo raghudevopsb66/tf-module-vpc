9d598d53d765dc8f18629e1f5c29b47aa6777096
Raghu K <raghuk.vit@gmail.com>
2022-09-12 08:11:59 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -17,3 +17,10 @@ module "subnets" {
   env      = var.env
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
--- subnets/main.tf
+++ subnets/main.tf
@@ -9,11 +9,5 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = var.vpc_id
 
-  tags = {
-    Name = "${var.env}-igw"
-  }
-}
 
