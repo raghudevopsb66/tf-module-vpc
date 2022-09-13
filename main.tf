5399ba83e259cd60a8ab6afd77117ece36c823e1
Raghu K <raghuk.vit@gmail.com>
2022-09-13 08:56:53 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -12,8 +12,6 @@ module "subnets" {
   subnets  = each.value["subnet_cidr"]
   vpc_id   = aws_vpc.main.id
   AZ       = var.AZ
-  ngw      = try(each.value["ngw"], false)
-  igw      = try(each.value["igw"], false)
   env      = var.env
   //igw_id   = aws_internet_gateway.igw.id
   //route_tables = aws_route_table.route-tables
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -2,6 +2,4 @@ variable "name" {}
 variable "subnets" {}
 variable "vpc_id" {}
 variable "AZ" {}
-variable "ngw" {}
-variable "igw" {}
 variable "env" {}
