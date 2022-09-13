947357c1ebc5cee2d972706406aa127d327fe5ff
Raghu K <raghuk.vit@gmail.com>
2022-09-13 09:02:26 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -57,12 +57,19 @@ resource "aws_nat_gateway" "ngw" {
   }
 }
 
-
 resource "aws_vpc_peering_connection" "peering-to-default-vpc" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.default_vpc_id
   auto_accept = true
 }
+
+resource "aws_route" "peering-route-on-default-route-table" {
+  route_table_id            = var.default_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering-to-default-vpc.id
+}
+
+
 
 
 
