de48a252f8953c6eb335428acd35e694636b1ccb
Raghu K <raghuk.vit@gmail.com>
2022-09-13 08:17:08 +0530

    Update tf-module-vpc/

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "routes" {
   source     = "./routes"
   vpc_id     = aws_vpc.main.id
   name       = each.value["name"]
-  subnet_ids = module.subnets.out[name]
+  subnet_ids = module.subnets.out[name].subnet_ids
 }
 
 output "out" {
