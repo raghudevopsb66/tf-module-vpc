063ac961a9179dcdf8c24ad9c40860643e334e76
Raghu K <raghuk.vit@gmail.com>
2022-09-13 07:53:59 +0530

    Update tf-module-vpc/

--- routes/main.tf
+++ routes/main.tf
@@ -5,8 +5,8 @@ resource "aws_route_table" "route-tables" {
   }
 }
 
-//resource "aws_route_table_association" "assoc" {
-//  count          = length(module.subnets["public"].out[*].id)
-//  subnet_id      = element(module.subnets["public"].out[*].id, count.index)
-//  route_table_id = aws_route_table.route-tables["public"].id
-//}
+resource "aws_route_table_association" "assoc" {
+  count          = length(var.subnet_ids[var.name].out[*].id)
+  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index)
+  route_table_id = aws_route_table.route-tables.id
+}
