1458e021d30b825e3bcd8e65dbb169453378632f
Raghu K <raghuk.vit@gmail.com>
2022-09-13 08:24:17 +0530

    Update tf-module-vpc/

--- routes/main.tf
+++ routes/main.tf
@@ -12,7 +12,7 @@ resource "aws_route_table" "route-tables" {
 //}
 
 resource "local_file" "foo" {
-  content  = jsonencode(var.subnet_ids[var.name]).subnet_ids
+  content  = length(jsonencode(var.subnet_ids[var.name]).subnet_ids)
   filename = "/tmp/out"
 }
 
